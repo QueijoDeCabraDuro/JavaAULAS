76140f49c6a240162356045a84d4c3ef24fe6211
QueijoDeCabraDuro <barracudafishy4@gmail.com>
2026-09-19 15:13:31 +0100

    Add files via upload
    Experiência com Java, Simples teste de println

---
+++ BancoDigitalMFJ.java
@@ -0,0 +1,12 @@
+public class BancoDigitalMFJ {
+    public static void main(String[] args){
+        System.out.println("=================");
+        System.out.println("Bem-vindo ao Banco Digital!");
+        System.out.println("Pressione 1 para depositar dinheiro");
+        System.out.println("Pressione 2 para retirar dinheiro");
+        System.out.println("Pressione 3 para pedir empréstimo");
+        System.out.println("Java Builders | SHARKCODERS");
+        System.out.println("=================");
+
+    }
+}
